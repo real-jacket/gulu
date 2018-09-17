e1e09b267bb0c51cfdf7e37c9664b70fa206f9b4
xiaomu-deepin <1762982273@qq.com>
2018-09-17 15:37:13 +0800

    使用chai.spy监听回调函数

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ new Vue({
 });
 
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies);
 
 const expect = chai.expect;
 //单元测试
@@ -90,9 +92,11 @@ const expect = chai.expect;
         }
     });
     vm.$mount();
+    let spy = chai.spy(function(){});
 
     vm.$on('click', spy);
     // 希望这个函数被执行
     let button = vm.$el;
     button.click();
+    expect(spy).to.have.been.called()
 }
