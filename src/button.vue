728079c4832cad08bc9bfe7760b9dec8aecf156a
xiaomu-deepin <1762982273@qq.com>
2018-09-18 22:28:59 +0800

    添加input.vue及css

--- src/button.vue
+++ src/button.vue
@@ -11,6 +11,7 @@
 <script>
     import Icon from './icon'
     export default {
+        name:'guluButton',
         components:{
           'g-icon': Icon
         },
---
+++ src/input.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="wrapper" :class="{error}">
+        <input type="text" :value="name" :disabled="disabled" :readonly="readonly" />
+        <template v-if="error">
+            <g-icon  name="error" class="icon-error"></g-icon>
+            <span class="errormessage">{{error}}</span>
+        </template>
+    </div>
+</template>
+<script>
+    import Icon from './icon'
+    export default {
+        components: {
+            Icon:'g-icon'
+        },
+        name:'guluIput',
+        props:{
+            name:{
+                type:String
+            },
+            disabled:{
+                type: Boolean,
+                default:false
+            },
+            readonly:{
+                type:Boolean,
+                default: false
+            },
+            error:{
+                type:String
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $font-size: 14px;
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $red: #F1453D;
+    .wrapper{
+        font-size: $font-size;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        > :not(:last-child){margin-right:0.5em;}
+        > input{
+            height: $height;
+            font-size: inherit;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            padding: 0 8px;
+            &:hover{border-color: $border-color-hover;}
+            &:focus{box-shadow: inset 0 1px 3px $box-shadow-color;outline: none;}
+            &[disabled],&[readonly]{border-color: #bbb;color: #bbb;
+                cursor: not-allowed;}
+        }
+        &.error{
+            > input{border-color:$red;}
+        }
+        .icon-error{fill:$red;}
+        .errormessage{color: $red;}
+    }
+</style>
