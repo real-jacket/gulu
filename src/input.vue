4649ce8c9e41ecbf25a5072b883f0cc34ec1a53c
xiaomu-deepin <1762982273@qq.com>
2018-09-19 10:56:45 +0800

    监听input Error事件

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="wrapper" :class="{error}">
-        <input type="text" :value="name" :disabled="disabled" :readonly="readonly" />
+        <input type="text" :value="name" :disabled="disabled" :readonly="readonly"
+            @change="$emit('change',$event.target.value)"
+            @input="$emit('input',$event.target.value)"
+            @focus="$emit('focus',$event.target.value)"
+            @blur="$emit('blur',$event.target.value)"
+        >
         <template v-if="error">
             <g-icon  name="error" class="icon-error"></g-icon>
             <span class="errormessage">{{error}}</span>
@@ -9,6 +14,7 @@
 </template>
 <script>
     import Icon from './icon'
+
     export default {
         components: {
             Icon:'g-icon'
