a6f62fdfb148e99c995d4fa2776c83638a3d8fb1
xiaomu-deepin <1762982273@qq.com>
2018-09-19 11:27:23 +0800

    添加v-model

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrapper" :class="{error}">
-        <input type="text" :value="name" :disabled="disabled" :readonly="readonly"
+        <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
             @change="$emit('change',$event.target.value)"
             @input="$emit('input',$event.target.value)"
             @focus="$emit('focus',$event.target.value)"
@@ -21,7 +21,7 @@
         },
         name:'guluIput',
         props:{
-            name:{
+            value:{
                 type:String
             },
             disabled:{
