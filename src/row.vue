8e2bbd6f9f3da3541f6e78b33915011ca6cf2e0c
xiaomu-deepin <1762982273@qq.com>
2018-09-19 17:38:27 +0800

    初步完成row col

---
+++ src/row.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'guluRow',
+        props:{
+            gutter:[Number,String]
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .row{
+        border: 1px solid red;
+        background: #aaaaaa;
+        display: flex;
+    }
+</style>
