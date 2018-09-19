0868e2c68feba453430f7fbdeac36c9338edd38b
xiaomu-deepin <1762982273@qq.com>
2018-09-19 19:01:22 +0800

    基本实现row、col功能

---
+++ src/col.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="col" :class="[span && `col-${span}`,offset &&  `offset-${offset}`]"
+    :style="{paddingLeft:gutter/2 + 'px',paddingRight: gutter/2 + 'px' }">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'guluRow',
+        props:{
+            span:{
+                type:[Number,String]
+            },
+            offset:{
+                type: [Number,String]
+            }
+        },
+        data(){
+            return {
+                gutter:0
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .col{
+        border: 1px solid green;
+        height: 32px;
+        width: 50%;
+
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n}{
+                width: ($n / 24) * 100%;
+            }
+        }
+
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n}{
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2 + 'px',marginRight:-gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
@@ -8,13 +8,16 @@
         name:'guluRow',
         props:{
             gutter:[Number,String]
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
         }
     }
 </script>
 <style lang="scss" scoped>
     .row{
-        border: 1px solid red;
-        background: #aaaaaa;
         display: flex;
     }
 </style>
