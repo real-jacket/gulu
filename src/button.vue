a377717123877c41f30cf4b2dfd8b588ccaaff2a
xiaomu-deepin <1762982273@qq.com>
2018-09-24 19:06:39 +0800

    将scss变量提升到_var.scss

--- src/button.vue
+++ src/button.vue
@@ -28,39 +28,40 @@
         }
     }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+    @import "var";
     @keyframes spin {
         0%{transform: rotate(0deg)}
         100%{transform: rotate(360deg)}
     }
     .g-button {
-        font-size: var(--font-size);
-        line-height: var(--font-size);
-        height: var(--button-height);
+        font-size: $font-size;
+        line-height: $font-size;
+        height: $button-height;
         padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+        border-radius: $border-radius;
+        border: 1px solid $border-color;
+        background: $button-bg;
         vertical-align: middle;
         display: inline-flex;
         justify-content: center;
         align-items: center;
 
         &:hover {
-            border-color: var(--border-color-hover);
+            border-color: $border-color-hover;
         }
         &:active {
-            background: var(--button-active-bg);
+            background: $button-active-bg;
         }
         &:focus {
             outline: none;
         }
 
-        >.content{order:2;}
+        >.g-content{order:2;}
         >.icon{order:1;margin-right: 0.3em}
 
         &.icon-right{
-            >.content{order: 1;}
+            >.g-content{order: 1;}
             >.icon{order: 2;margin-left:0.3em;margin-right: 0;}
         }
         .loading{
