2f14cc87cb0a6dd6d07e9efe41c11d924804e5c9
xiaomu-deepin <1762982273@qq.com>
2018-09-25 16:49:06 +0800

    在npm上发布更新版本0.0.5

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from  './src/icon'
+import Vue from 'vue'
+import Input from './src/input'
+import Row from './src/row'
+import Col from './src/col'
+import Layout from './src/layout'
+import Header from './src/header'
+import Sider from './src/sider'
+import Content from './src/content'
+import Footer from './src/footer'
+import Toast from './src/toast'
+import Plugin from './src/plugin'
+import Tabs from './src/tabs'
+import TabsNav from './src/tabs-nav'
+import TabsItem from './src/tabs-item'
+import TabsBody from './src/tabs-body'
+import TabsPane from './src/tabs-pane'
 
-export {ButtonGroup,Button,Icon}
+export {ButtonGroup,Button,Icon,Input,Row,Col,Layout,Header,Sider,Content,Footer,TabsNav,
+    Toast,Plugin,TabsPane,TabsBody,Tabs,TabsItem,Plugin,Vue}
+
